8337f29cb93962aa253d2606e1ad6ac72feddcf9
Lori Culberson <bigskybandit@sbcglobal.net>
2016-09-05 21:38:50 -0500

    Implemented lap button functionality and styling

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,10 @@ var {
 var StopWatch = React.createClass({ 
   getInitialState: function() {
     return {
-      timeElapsed: null
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: []
     }
   },
   render: function(){
@@ -30,41 +33,71 @@ var StopWatch = React.createClass({
       </View>
 
       <View style={[styles.footer]}> 
-        <Text>
-          Lap list
-        </Text>
+        {this.laps()}
       </View>
   </View>
   },
+  laps: function(){
+    return this.state.laps.map(function(time, index){
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
+          Lap: {index + 1}
+        </Text>
+        <Text style={styles.lapText}>
+          {formatTime(time)}
+        </Text>
+      </View>
+    });
+  },
   startStopButton: function(){
+    var style = this.state.running ? styles.stopButton : styles.startButton;
     return <TouchableHighlight 
       underlayColor='gray'
       onPress={this.handleStartPress}
-      style={[styles.button, styles.startButton]}
+      style={[styles.button, style]}
       >
 
       <Text>
-        Start
+        {this.state.running ? 'Stop' : 'Start'}
       </Text>
     </TouchableHighlight>
   },
-  lapButton: function(){
-    return <View style={styles.button}>
+  lapButton: function() {
+    return <TouchableHighlight 
+      style={styles.button}
+      underlayColor='gray'
+      onPress={this.handleLapPress}
+    >
       <Text>
         Lap
       </Text>
-    </View>
+    </TouchableHighlight>
   },
   handleStartPress: function() {
-    var startTime = new Date();
+    if(this.state.running){
+      clearInterval(this.interval);
+      this.setState({running: false});
+      return
+    }
 
-    setInterval(() => {
+    var startTime = new Date();
+    this.setState({startTime: new Date()}); 
+
+    this.interval = setInterval(() => {
       this.setState({
-        timeElapsed: new Date() - startTime 
+        timeElapsed: new Date() - this.state.startTime,
+        running: true 
       });
     }, 30)
 
-  }
+  },
+  handleLapPress: function() {
+    var lap = this.state.timeElapsed;
+    this.setState({
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap])
+    });
+  }, 
 });
 
 var styles = StyleSheet.create({
@@ -102,6 +135,16 @@ var styles = StyleSheet.create({
   },
   startButton: {
     borderColor: '#00cc00'
+  },
+  stopButton: {
+    borderColor: '#ff0000'
+  },
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+  lapText: {
+    fontSize: 30
   }
 });
 
